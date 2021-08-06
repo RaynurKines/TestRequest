c163473bf0ca396e1a25e81ee02be3ed29b71c27
RaynurKines <kinespaev.raynur@gmail.com>
2021-08-06 13:26:24 +0600

    fix

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,6 @@ package com.company;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.ContentType;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
@@ -14,9 +13,11 @@ public class Main {
         CloseableHttpClient httpclient = HttpClients.createDefault();
         HttpPost httpPost = new HttpPost("http://api.shoutcloud.io/V1/SHOUT");
 
-        String inputJson = "{" + " \"input\":\"Hello\"" + "}";
-        StringEntity stringEntity = new StringEntity(inputJson, ContentType.APPLICATION_JSON);
+        String inputJson = "{\"input\":\"Hello\"}";
+        StringEntity stringEntity = new StringEntity(inputJson);
         httpPost.setEntity(stringEntity);
+        httpPost.setHeader("Accept", "application/json");
+        httpPost.setHeader("Content-type", "application/json");
         HttpResponse response = httpclient.execute(httpPost);
         System.out.println(response.getStatusLine());
         System.out.println(response.getEntity().getContent());
